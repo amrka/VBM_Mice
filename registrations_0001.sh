c5e470f44e73283e389f1bdd43e63beb5725ce30
amrka <amr.kamel@uv.es>
2020-06-21 18:21:05 +0200

    add the correct transofrmation file

--- registrations_0001.sh
+++ registrations_0001.sh
@@ -17,3 +17,29 @@ antsRegistrationSyN.sh \
 -f TMBTA_Brain_Template.nii \
 -n 16 \
 -o VBM_to_TMBTA_
+
+
+#now we back-register the TMBTA priors to our template space
+antsApplyTransforms \
+-d 3 \
+-i TMBTA_Grey.nii \
+-r VBM_template_manual_ext.nii.gz \
+-o GM_to_VBM.nii.gz \
+-t VBM_to_TMBTA_InverseComposite.h5 \
+-v
+
+antsApplyTransforms \
+-d 3 \
+-i TMBTA_White.nii \
+-r VBM_template_manual_ext.nii.gz \
+-o WM_to_VBM.nii.gz \
+-t VBM_to_TMBTA_InverseComposite.h5 \
+-v
+
+antsApplyTransforms \
+-d 3 \
+-i TMBTA_CSF.nii \
+-r VBM_template_manual_ext.nii.gz \
+-o CSF_to_VBM.nii.gz \
+-t VBM_to_TMBTA_InverseComposite.h5 \
+-v
