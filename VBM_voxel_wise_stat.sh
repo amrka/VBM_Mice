5bfdd77b1d3b752eee0a1803fa1a5c3c51c54321
amrka <amr.kamel@uv.es>
2020-06-23 02:28:18 +0200

    try also with FWHM 2

--- VBM_voxel_wise_stat.sh
+++ VBM_voxel_wise_stat.sh
@@ -24,17 +24,55 @@ python ~/SCRIPTS/change_files_to_contain_gp_name.py \
 fslmerge \
 -t \
 /home/in/aeed/Work/October_Acquistion/VBM/VBM_stats/VBM_FWHM_3_mod_GM.nii.gz \
-/home/in/aeed/Work/October_Acquistion/VBM/VBM_stats/*.nii.gz
+/home/in/aeed/Work/October_Acquistion/VBM/VBM_stats/*_smooth_3_mod_GM.nii.gz
 
 #5 create the stat design
 design_ttest2 /home/in/aeed/Work/October_Acquistion/VBM/VBM_stats/design_VBM 16 16
 
 #6 run randomise
-cd design_ttest2 /home/in/aeed/Work/October_Acquistion/VBM/VBM_stats/design_VBM 16 16
+cd /home/in/aeed/Work/October_Acquistion/VBM/VBM_stats
 
-randomise_parallel \
--i VBM_FWHM_3_mod_GM.nii.gz \
--o VBM_FWHM_3 \
+randomise \
+-i /home/in/aeed/Work/October_Acquistion/VBM/VBM_stats/VBM_FWHM_3_mod_GM.nii.gz \
+-o /home/in/aeed/Work/October_Acquistion/VBM/VBM_stats/VBM_FWHM_3_10000 \
 -m /home/in/aeed/Work/October_Acquistion/VBM/registration/VBM_template_manual_ext_mask.nii.gz  \
--d design_VBM.mat -t design_VBM.con \
+-d /home/in/aeed/Work/October_Acquistion/VBM/VBM_stats/design_VBM.mat \
+-t /home/in/aeed/Work/October_Acquistion/VBM/VBM_stats/design_VBM.con \
+-n 10000 --uncorrp -T -V;
+
+#----------------------------------------------------------------------------------------------
+# try the same with fwhm smoothing
+
+cd ~/Work/October_Acquistion/Data
+
+mkdir ~/Work/October_Acquistion/VBM/VBM_stats/smooth_2
+
+for folder in *;do
+    cp ~/Work/October_Acquistion/VBM/VBM_workingdir_preproc/VBM_workflow/_subject_id_${folder}/_fwhm_2/Smoothing/Modulated_GM_smooth.nii.gz \
+    ~/Work/October_Acquistion/VBM/VBM_stats//smooth_2/${folder}_smooth_2_mod_GM.nii.gz;
+done
+
+#3 change the file names to contain group
+
+python ~/SCRIPTS/change_files_to_contain_gp_name.py \
+/home/in/aeed/Work/October_Acquistion/VBM/VBM_stats/smooth_2 0 3
+
+#4 merge
+fslmerge \
+-t \
+/home/in/aeed/Work/October_Acquistion/VBM/VBM_stats/smooth_2/VBM_FWHM_2_mod_GM.nii.gz \
+/home/in/aeed/Work/October_Acquistion/VBM/VBM_stats/smooth_2/*_smooth_2_mod_GM.nii.gz
+
+#5 create the stat design
+design_ttest2 /home/in/aeed/Work/October_Acquistion/VBM/VBM_stats/smooth_2/design_VBM 16 16
+
+#6 run randomise
+cd /home/in/aeed/Work/October_Acquistion/VBM/VBM_stats/smooth_2
+
+randomise \
+-i /home/in/aeed/Work/October_Acquistion/VBM/VBM_stats/smooth_2/VBM_FWHM_2_mod_GM.nii.gz \
+-o /home/in/aeed/Work/October_Acquistion/VBM/VBM_stats/smooth_2/VBM_FWHM_2_10000 \
+-m /home/in/aeed/Work/October_Acquistion/VBM/registration/VBM_template_manual_ext_mask.nii.gz  \
+-d /home/in/aeed/Work/October_Acquistion/VBM/VBM_stats/smooth_2/design_VBM.mat \
+-t /home/in/aeed/Work/October_Acquistion/VBM/VBM_stats/smooth_2/design_VBM.con \
 -n 10000 --uncorrp -T -V;
