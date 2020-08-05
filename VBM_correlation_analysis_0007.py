56d0d42f9432c5aab351ed6baf929c2da8eadebd
amrka <amr.kamel@uv.es>
2020-08-05 16:19:42 +0200

    remove G

--- VBM_correlation_analysis_0007.py
+++ VBM_correlation_analysis_0007.py
@@ -235,5 +235,5 @@ VBM_corr.connect ([
 
 
 VBM_corr.write_graph(graph2use='colored', format='svg', simple_form=True)
-VBM_corr.run(plugin='SLURM', plugin_args={'dont_resubmit_completed_jobs': True,'max_jobs':50, '--mem':16G})
+VBM_corr.run(plugin='SLURM', plugin_args={'dont_resubmit_completed_jobs': True,'max_jobs':50, '--mem':16000})
 # VBM_corr.run('MultiProc', plugin_args={'n_procs': 8})
