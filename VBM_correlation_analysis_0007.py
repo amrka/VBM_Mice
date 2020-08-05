57e04f12f5fb801253043e4a0023c68255053f63
amrka <amr.kamel@uv.es>
2020-08-05 12:49:05 +0200

    finishing the script and run a trial with 10 perms only

--- VBM_correlation_analysis_0007.py
+++ VBM_correlation_analysis_0007.py
@@ -52,7 +52,10 @@ datasink = Node(DataSink(), name = 'datasink')
 datasink.inputs.container = output_dir
 datasink.inputs.base_directory = experiment_dir
 
-substitutions = [('_map_id_', '')]
+substitutions = [('_map_id_', ''),
+('_contrast_..media..amr..Amr_4TB..Work..October_Acquistion..VBM..VBM_corr_designs..', ''),
+('design_..media..amr..Amr_4TB..Work..October_Acquistion..VBM..VBM_corr_designs..', ''),
+]
 
 datasink.inputs.substitutions = substitutions
 
@@ -76,7 +79,8 @@ designs = [
 '/media/amr/Amr_4TB/Work/October_Acquistion/VBM/VBM_corr_designs/OF_sec_in_center.mat',
 '/media/amr/Amr_4TB/Work/October_Acquistion/VBM/VBM_corr_designs/OF_total_distance.mat',
 '/media/amr/Amr_4TB/Work/October_Acquistion/VBM/VBM_corr_designs/OF_total_time_in_corners.mat',
-'/media/amr/Amr_4TB/Work/October_Acquistion/VBM/VBM_corr_designs/OF_velocity.mat']
+'/media/amr/Amr_4TB/Work/October_Acquistion/VBM/VBM_corr_designs/OF_velocity.mat'
+]
 
 contrasts = [
 '/media/amr/Amr_4TB/Work/October_Acquistion/VBM/VBM_corr_designs/EPM_open_to_close_ratio.con',
@@ -94,7 +98,8 @@ contrasts = [
 '/media/amr/Amr_4TB/Work/October_Acquistion/VBM/VBM_corr_designs/OF_sec_in_center.con',
 '/media/amr/Amr_4TB/Work/October_Acquistion/VBM/VBM_corr_designs/OF_total_distance.con',
 '/media/amr/Amr_4TB/Work/October_Acquistion/VBM/VBM_corr_designs/OF_total_time_in_corners.con',
-'/media/amr/Amr_4TB/Work/October_Acquistion/VBM/VBM_corr_designs/OF_velocity.con']
+'/media/amr/Amr_4TB/Work/October_Acquistion/VBM/VBM_corr_designs/OF_velocity.con'
+]
 
 
 #-----------------------------------------------------------------------------------------------------
@@ -112,11 +117,14 @@ def palm_corr(in_file, mask, design, contrast):
 
 
     cl = CommandLine(cmd.format(in_file=in_file, mask=mask, design=design, contrast=contrast ))
-    results = cl.run()
+    cl.run()
+    tstat1 = os.path.abspath('palm_corr_vbm_vox_tstat_c1.nii.gz')
+    tstat2 = os.path.abspath('palm_corr_vbm_vox_tstat_c2.nii.gz')
+    return tstat1, tstat2
 
 palm_corr = Node(name = 'palm_corr',
                  interface = Function(input_names = ['in_file', 'mask', 'design', 'contrast'],
-                                        output_names = ['results'],
+                                        output_names = ['tstat1', 'tstat2'],
                                       function = palm_corr))
 
 
@@ -124,15 +132,98 @@ palm_corr.iterables = [("design", designs),("contrast", contrasts)]
 palm_corr.synchronize = True # synchronize here serves to make sure design and contrast are used in pairs
 # Not using all the possible permuatations
 #-----------------------------------------------------------------------------------------------------
+# use the tstat maps to calculate r-pearson correlation coeeficient
+# >>> fslmaths tstat.nii.gz -sqr tstat2.nii.gz
+# >>> fslmaths tstat.nii.gz -abs -div tstat.nii.gz sign.nii.gz
+# >>> fslmaths tstat2.nii.gz -add DF denominator.nii.gz
+# >>> fslmaths tstat2.nii.gz -div denominator.nii.gz -sqrt -mul sign.nii.gz correlation.nii.gz
+square1 = Node(fsl.UnaryMaths(), name='square1')
+square1.inputs.operation = 'sqr'
+square1.inputs.out_file = 'tstat1_squared.nii.gz'
+
+sign_t1 = Node(fsl.ImageMaths(), name='sign_t1')
+sign_t1.inputs.op_string = '-abs -div'
+sign_t1.inputs.out_file = 'sign_tstat1.nii.gz'
+
+add_df1 = Node(fsl.BinaryMaths(), name='add_df1')
+add_df1.inputs.operation = 'add'
+add_df1.inputs.operand_value = 30 #32 animals-2contrast = 30 dof
+add_df1.inputs.out_file = 'denominator_tstat1.nii.gz'
+
+div_by_denom1 = Node(fsl.BinaryMaths(), name='div_by_denom1')
+div_by_denom1.inputs.operation = 'div'
+div_by_denom1.inputs.out_file = 'divided_by_denominator.nii.gz'
+
+create_corr1 = Node(fsl.ImageMaths(), name='create_corr1')
+create_corr1.inputs.op_string = '-sqrt -mul'
+create_corr1.inputs.out_file = 'corr_coef_r1.nii.gz'
+#-----------------------------------------------------------------------------------------------------
+# same thing with tstat2 to get r2
+square2 = Node(fsl.UnaryMaths(), name='square2')
+square2.inputs.operation = 'sqr'
+square2.inputs.out_file = 'tstat2_squared.nii.gz'
+
+sign_t2 = Node(fsl.ImageMaths(), name='sign_t2')
+sign_t2.inputs.op_string = '-abs -div'
+sign_t2.inputs.out_file = 'sign_tstat2.nii.gz'
+
+add_df2 = Node(fsl.BinaryMaths(), name='add_df2')
+add_df2.inputs.operation = 'add'
+add_df2.inputs.operand_value = 30 #32 animals-2contrast = 30 dof
+add_df2.inputs.out_file = 'denominator_tstat2.nii.gz'
+
+div_by_denom2 = Node(fsl.BinaryMaths(), name='div_by_denom2')
+div_by_denom2.inputs.operation = 'div'
+div_by_denom2.inputs.out_file = 'divided_by_denominator.nii.gz'
+
+create_corr2 = Node(fsl.ImageMaths(), name='create_corr2')
+create_corr2.inputs.op_string = '-sqrt -mul'
+create_corr2.inputs.out_file = 'corr_coef_r2.nii.gz'
+
+
+#-----------------------------------------------------------------------------------------------------
 VBM_corr.connect ([
 
       (infosource, selectfiles,[('map_id','map_id')]),
 
       (selectfiles, palm_corr, [('VBM','in_file')]),
       (selectfiles, palm_corr, [('VBM_mask','mask')]),
+
+      (palm_corr, datasink, [('tstat1','tstat1')]),
+      (palm_corr, datasink, [('tstat2','tstat2')]),
+#==================================r1==============================================
+      (palm_corr, square1, [('tstat1','in_file')]),
+
+      (palm_corr, sign_t1, [('tstat1','in_file')]),
+      (palm_corr, sign_t1, [('tstat1','in_file2')]),
+
+      (square1, add_df1, [('out_file','in_file')]),
+
+      (square1, div_by_denom1, [('out_file','in_file')]),
+      (add_df1, div_by_denom1, [('out_file','operand_file')]),
+
+      (div_by_denom1, create_corr1, [('out_file','in_file')]),
+      (sign_t1, create_corr1, [('out_file','in_file2')]),
+
+      (create_corr1, datasink, [('out_file','corr_coef_r1')]),
+#==================================r2==============================================
+      (palm_corr, square2, [('tstat2','in_file')]),
+
+      (palm_corr, sign_t2, [('tstat2','in_file')]),
+      (palm_corr, sign_t2, [('tstat2','in_file2')]),
+
+      (square2, add_df2, [('out_file','in_file')]),
+
+      (square2, div_by_denom2, [('out_file','in_file')]),
+      (add_df2, div_by_denom2, [('out_file','operand_file')]),
+
+      (div_by_denom2, create_corr2, [('out_file','in_file')]),
+      (sign_t2, create_corr2, [('out_file','in_file2')]),
+
+      (create_corr2, datasink, [('out_file','corr_coef_r2')]),
   ])
 
 
-VBM_corr.write_graph(graph2use='colored', format='png', simple_form=True)
+VBM_corr.write_graph(graph2use='colored', format='svg', simple_form=True)
 # VBM_corr.run(plugin='SLURM', plugin_args={'dont_resubmit_completed_jobs': True,'max_jobs':50})
-VBM_corr.run('MultiProc', plugin_args={'n_procs': 4})
+VBM_corr.run('MultiProc', plugin_args={'n_procs': 8})
