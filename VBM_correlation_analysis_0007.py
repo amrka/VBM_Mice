833e3b2b2d0e8486dc7d5e4d8a139d578469205b
amrka <amr.kamel@uv.es>
2020-08-05 16:27:59 +0200

    useful commetn about sbatch args

--- VBM_correlation_analysis_0007.py
+++ VBM_correlation_analysis_0007.py
@@ -236,4 +236,5 @@ VBM_corr.connect ([
 
 VBM_corr.write_graph(graph2use='colored', format='svg', simple_form=True)
 VBM_corr.run(plugin='SLURM', plugin_args={'dont_resubmit_completed_jobs': True,'max_jobs':50, '--mem':16000})
+# plugin_args={'sbatch_args': '--time=24:00:00 -N1 -c2 --mem=40G','max_jobs':200}
 # VBM_corr.run('MultiProc', plugin_args={'n_procs': 8})
