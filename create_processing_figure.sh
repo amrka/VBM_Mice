22cd083797ce174cca9c68fb056e624497e18be9
Amr <amr.kamel@uv.es>
2020-10-22 11:09:26 +0200

    assign different colormap to the atlas

--- create_processing_figure.sh
+++ create_processing_figure.sh
@@ -64,7 +64,20 @@
 # make a directory
 mkdir /Users/amr/Dropbox/thesis/VBM_study_temp
 
-# I copied the files manually already
+# copy the necessary files except for the atlas, because it needs random colormap
+
+imcp /Volumes/Amr_1TB/VBM/registration/CSF_to_VBM.nii.gz \
+/Users/amr/Dropbox/thesis/VBM_study_temp
+
+imcp /Volumes/Amr_1TB/VBM/registration/GM_to_VBM.nii.gz \
+/Users/amr/Dropbox/thesis/VBM_study_temp
+
+imcp /Volumes/Amr_1TB/VBM/registration/WM_to_VBM.nii.gz \
+/Users/amr/Dropbox/thesis/VBM_study_temp
+
+imcp /Volumes/Amr_1TB/VBM/registration/VBM_template_manual_ext.nii.gz \
+/Users/amr/Dropbox/thesis/VBM_study_temp
+
 
 cd /Users/amr/Dropbox/thesis/VBM_study_temp
 
@@ -94,3 +107,30 @@ for img in *.ni*;do
 	convert ${img_name}_output.png -transparent black ${img_name}_output_no_bg.png
 
 done
+
+imcp /Volumes/Amr_1TB/VBM/registration/Atlas_to_VBM.nii.gz \
+/Users/amr/Dropbox/thesis/VBM_study_temp
+
+
+img=Atlas_to_VBM.nii.gz
+img_name=`remove_ext $img`
+
+for slice_no in {130,104,78,52,26};do
+
+	${fsleyes} render  \
+	--scene ortho -no   --displaySpace world --hidex --hidez  --hideLabels  -vl 54 ${slice_no} 39 \
+	--hideCursor   --outfile    ${img_name}_${slice_no}.png   $img  --cmap random
+	 
+done
+
+pngappend \
+${img_name}_26.png + 0 \
+${img_name}_52.png + 0 \
+${img_name}_78.png + 0 \
+${img_name}_104.png + 0 \
+${img_name}_130.png  \
+${img_name}_output.png
+
+# remove background
+convert ${img_name}_output.png -transparent black ${img_name}_output_no_bg.png
+
